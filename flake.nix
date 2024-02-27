e392bd51219baa53ab9883b6998b7957d9913c42
sem pruijs <sem@pruijs.net>
2024-02-27 19:55:14 +0100

    init files from server

--- flake.nix
+++ flake.nix
@@ -1,31 +1,16 @@
 {
-  description = "cardano stake pool";
-
+  description = "A simple NixOS flake";
+  
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+  }; 
 
-    cardano-node.url = "github:intersectmbo/cardano-node";
-  };
-  # pass inputs to output function
-  outputs = inputs@{ self, nixpkgs, flake-parts, cardano-node, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems =
-        [ "x86_64-linux" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }:
-        let
-        in {
-          packages = {
-            # add build phases here
-            # default = 
-            cardano-node = inputs.cardano-node.packages.${system}.cardano-node;
-            cardano-cli = inputs.cardano-node.packages.${system}.cardano-cli;
-          };
-          devShells = {
-            default = pkgs.mkShell {
-              buildInputs = with pkgs; [ ponysay ];
-            };
-          };
-        };
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations = {
+      hostName = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+     };
     };
+  };
 }
