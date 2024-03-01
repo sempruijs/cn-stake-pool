f02cfa20a4df26f6dd346f81f6d5a756d1b3549c
sem pruijs <sem@pruijs.net>
2024-03-01 22:23:37 +0100

    save state for cor

--- flake.nix
+++ flake.nix
@@ -3,14 +3,28 @@
   
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   }; 
 
-  outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations = {
-      hostName = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
-     };
-    };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+      flake-parts.lib.mkFlake { inherit inputs; } {
+        systems =
+          [ "x86_64-linux" "aarch64-linux" ];
+        perSystem = { config, self', inputs', pkgs, system, ... }:
+          let
+            crane = rec {
+              lib = self.inputs.crane.lib.${system};
+              stable = lib.overrideToolchain self'.packages.rust-stable;
+            };
+          in {
+            packages = {
+              nixosConfigurations = {
+                hostName = nixpkgs.lib.nixosSystem {
+                  system = "x86_64-linux";
+                  modules = [ ./configuration.nix ];
+               };
+              };
+            };
+          };
   };
 }
